b667d027bb45038d2a05a780dceef265b63f2aeb
KANGER-XD <91680391+KANGER-XD@users.noreply.github.com>
2021-11-06 17:18:51 +0530

    .

--- MissRoseSong_Bot.py
+++ MissRoseSong_Bot.py
@@ -90,7 +90,7 @@ def a(client, message):
         return
     m.edit("⏬ Downloading.")
     try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        with yt-dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
